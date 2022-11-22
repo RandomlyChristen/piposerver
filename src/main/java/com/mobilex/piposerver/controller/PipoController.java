d6be20994d64a34c824dee70759b7442ebd04dfb
RandomlyChristen <yylwd2000@naver.com>
2022-11-23 01:59:19 +0900

    jwt parse exception handled

--- src/main/java/com/mobilex/piposerver/controller/PipoController.java
+++ src/main/java/com/mobilex/piposerver/controller/PipoController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -27,7 +28,7 @@ import java.net.URLConnection;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("pipo")
 public class PipoController {
@@ -50,23 +51,23 @@ public class PipoController {
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().data(newEntity).build();
             return ResponseEntity.ok().body(responseDTO);
         }
-        catch (MultipartFileException e) {
+        catch (MultipartFileException | MultipartException e) {
             log.debug(TAG, e);
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().error("file is not available image").build();
-            return ResponseEntity.badRequest().body(responseDTO);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
         }
         catch (IOException e) {
             log.error(TAG, e);
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().error("internal server error").build();
-            return ResponseEntity.internalServerError().body(responseDTO);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
         } catch (UserNotFoundException e) {
             log.debug(TAG, e);
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().error("cannot found user from auth").build();
-            return ResponseEntity.badRequest().body(responseDTO);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
         } catch (JwtException e) {
             log.debug(TAG, e);
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().error("invalid JWT").build();
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseDTO);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
         }
     }
 
@@ -187,7 +188,7 @@ public class PipoController {
     @Nullable
     private UserEntity getUserEntityFromHeaderAuth(MultiValueMap<String, String> headers) throws UserNotFoundException, JwtException {
         String jwtString = headers.getFirst("authorization");
-        if (jwtString == null) return null;
+        if (jwtString == null || jwtString.length() < 8) return null;
         jwtString = jwtString.substring(7);
         return userService.getUserFromJwtString(jwtString);
     }
